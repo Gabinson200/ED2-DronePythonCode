068e924464670f516e0056868657f5c6b3452ccc
Gabinson200 <42674620+Gabinson200@users.noreply.github.com>
2021-06-12 21:34:13 -0400

    Create imageCapture.py

---
+++ helper_functions/imageCapture.py
@@ -0,0 +1,17 @@
+  
+from djitellopy import tello
+import cv2
+
+#create a tello obj and connect drone
+me = tello.Tello()
+me.connect()
+print(me.get_battery())
+
+me.stream_on()
+
+while True:
+    img = me.get_frame_read().frame
+    
+    img = cv2.resize(img, (360, 240))
+    cv2.imshow("Img", img)
+    cv2.waitKey(1)
